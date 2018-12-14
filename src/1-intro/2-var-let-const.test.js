1f21cfc250790a1e07c1c393cd27d3f6085f77d4
Serina Ko <serinako08@gmail.com>
2018-12-14 15:16:22 +1100

    first commit

--- src/1-intro/2-var-let-const.test.js
+++ src/1-intro/2-var-let-const.test.js
@@ -1,21 +1,21 @@
 describe('var, let & const', function() {
   test('should understand var', function() {
     var a = 1;
-    expect(a).toBe(__);
+    expect(a).toBe(1);
     if (a) {
       var a = 2;
-      expect(a).toBe(__);
+      expect(a).toBe(2);
     }
-    expect(a).toBe(__);
+    expect(a).toBe(2);
   });
   test('should understand let', function() {
     let a = 1;
-    expect(a).toBe(__);
+    expect(a).toBe(1);
     if (a) {
       let a = 2;
-      expect(a).toBe(__);
+      expect(a).toBe(2);
     }
-    expect(a).toBe(__);
+    expect(a).toBe(1);
   });
   test('should understand const', function() {
     const a = 1;
