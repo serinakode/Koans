0ead0ce85b3c37c60f99d4b7cae1578cfaaf99db
Serina Ko <serinako08@gmail.com>
2018-12-17 09:40:58 +1100

    completed

--- src/2-objects/1-object-literals.test.js
+++ src/2-objects/1-object-literals.test.js
@@ -1,6 +1,6 @@
 describe('Object literals', function() {
   test('1 - should make sure that tests are setup correctly', function() {
-    expect(1).toBe(__);
+    expect(1).toBe(1);
   });
   test('2 - should understand object literals', function() {
     const samurai = {
@@ -17,14 +17,16 @@ describe('Object literals', function() {
         return 'Hello';
       }
     };
-    expect(typeof samurai).toBe(__);
+    expect(typeof samurai).toBe('object');
   });
+
   test('3 - shorthand syntax', function() {
     const name = 'Myamoto';
     const age = 32;
-    const samurai = { name, age };
-    expect(samurai).toEqual(__);
+    const samurai = { name, age }; // shorthand syntax
+    expect(samurai).toEqual({ name: 'Myamoto', age: 32 }); // full syntax (key, value)
   });
+
   test('4 - computed property names', function() {
     const propertyName = 'name';
     const name = 'Myamoto';
@@ -34,6 +36,10 @@ describe('Object literals', function() {
       ['first' + propertyName]: 'firstName',
       age
     };
-    expect(samurai).toEqual(__);
+    expect(samurai).toEqual({
+      name: 'Myamoto',
+      firstname: 'firstName',
+      age: 32
+    });
   });
 });
