823a7655ded935ba765b8d646bdca7618f8eff21
Serina Ko <serinako08@gmail.com>
2018-12-17 09:40:21 +1100

    completed

--- src/1-intro/2-var-let-const.test.js
+++ src/1-intro/2-var-let-const.test.js
@@ -1,3 +1,6 @@
+// VAR based variables are function scoped. We can redefine a variable and reassign a variable.
+// LET & CONST are blocked scope which does include functions. LET can only reassign a variable. CONST can't do any of them.
+
 describe('var, let & const', function() {
   test('should understand var', function() {
     var a = 1;
@@ -19,32 +22,35 @@ describe('var, let & const', function() {
   });
   test('should understand const', function() {
     const a = 1;
-    expect(a).toBe(__);
+    expect(a).toBe(1);
     if (a) {
       const a = 2;
-      expect(a).toBe(__);
+      expect(a).toBe(2);
     }
-    expect(a).toBe(__);
+    expect(a).toBe(1);
   });
   test('should understand let', function() {
     let a = 1;
-    expect(a).toBe(__);
+    expect(a).toBe(1);
     a = 2;
-    expect(a).toBe(__);
+    expect(a).toBe(2);
   });
   test('should understand const', function() {
     const a = 1;
-    expect(a).toBe(__);
+    expect(a).toBe(1);
     try {
       // a = 2;
     } catch (e) {
       //empty
     }
-    expect(a).toBe(__);
+    expect(a).toBe(1);
   });
+
   test('should understand const', function() {
     const a = { name: 'Myamoto' };
     a.name = 'Hattori';
-    expect(a).toEqual(__);
+    expect(a).toEqual({ name: 'Hattori' });
   });
+
+  // modifying it, so the result is different in that case.
 });
