f6fd0e6576a54aa459223244862779a477d965a6
Serina Ko <serinako08@gmail.com>
2018-12-17 15:35:25 +1100

    add logic to return true or false depending on the number of live neighbours

--- src/2-objects/9-game-of-life.js
+++ src/2-objects/9-game-of-life.js
@@ -1,5 +1,22 @@
 function isCellAliveInNextGeneration(isCellAlive, numberOfLiveNeighbours) {
   //TODO - Implement one test at a time
+
+  if (numberOfLiveNeighbours > 3) {
+    return (isCellAlive = false);
+  } else if (numberOfLiveNeighbours < 2) {
+    return (isCellAlive = false);
+  }
+
+  if (!isCellAlive) {
+    if (numberOfLiveNeighbours == 3) {
+      return true;
+    } else if (numberOfLiveNeighbours < 3 || numberOfLiveNeighbours > 3)
+      return false;
+  }
+
+  if (numberOfLiveNeighbours == 2 || numberOfLiveNeighbours == 3) {
+    return (isCellAlive = true);
+  }
 }
 
 export default isCellAliveInNextGeneration;
