87b5c35a5c039c472b4b41c17fc815d984e68240
Serina Ko <serinako08@gmail.com>
2018-12-18 10:32:29 +1100

    completed

--- src/2-objects/9-game-of-life.js
+++ src/2-objects/9-game-of-life.js
@@ -1,13 +1,13 @@
 function isCellAliveInNextGeneration(isCellAlive, numberOfLiveNeighbours) {
   //TODO - Implement one test at a time
 
-  if (numberOfLiveNeighbours < 2 || numberOfLiveNeighbours > 3) {
-    return (isCellAlive = false);
+  if (numberOfLiveNeighbours > 3 || numberOfLiveNeighbours < 2) {
+    isCellAlive = false;
   }
 
   if (!isCellAlive) {
     if (numberOfLiveNeighbours == 3) {
-      return true;
+      true;
     } else if (numberOfLiveNeighbours < 3 || numberOfLiveNeighbours > 3)
       return false;
   }
@@ -18,3 +18,29 @@ function isCellAliveInNextGeneration(isCellAlive, numberOfLiveNeighbours) {
 }
 
 export default isCellAliveInNextGeneration;
+
+// function isCellAliveInNextGeneration(isCellAlive, numberOfLiveNeighbours) {
+//TODO - Implement one test at a time
+// let truthy/falsy value => minimize return
+// initialise declaration
+//   let CellAlive = false;
+
+//   if (numberOfLiveNeighbours > 3 || numberOfLiveNeighbours < 2) {
+//     CellAlive;
+//   }
+
+//   if (!isCellAlive) {
+//     if (numberOfLiveNeighbours == 3) {
+//       CellAlive = true;
+//     } else if (numberOfLiveNeighbours < 3 || numberOfLiveNeighbours > 3) {
+//       CellArive;
+//     }
+//   }
+
+//   if (numberOfLiveNeighbours == 2 || numberOfLiveNeighbours == 3) {
+//     CellAlive = true;
+//   }
+//   return CellAlive;
+// }
+
+// export default isCellAliveInNextGeneration;
