245da5f25ece21ae21c0d6394c5efced49b09d8a
Serina Ko <serinako08@gmail.com>
2018-12-18 10:32:47 +1100

    completed

--- src/3-functions-pt1/1-function-expression-function-declaration.test.js
+++ src/3-functions-pt1/1-function-expression-function-declaration.test.js
@@ -1,49 +1,49 @@
 describe('function expression and function declaration', function() {
   test('1 - should understand function expression', function() {
-    expect(typeof f).toBe(__);
+    expect(typeof f).toBe('undefined');
     var f = function() {
       return 123;
     };
     const g = f;
-    expect(typeof f).toBe(__);
-    expect(f()).toBe(__);
-    expect(f === g).toBe(__);
+    expect(typeof f).toBe('function');
+    expect(f()).toBe(123);
+    expect(f === g).toBe(true);
   });
   test('2 - should understand function declaration', function() {
-    expect(typeof f).toBe(__);
+    expect(typeof f).toBe('function');
     function f() {
       return 123;
     }
-    expect(typeof f).toBe(__);
-    expect(f()).toBe(__);
+    expect(typeof f).toBe('function');
+    expect(f()).toBe(123);
   });
   test('3 - should understand function declaration', function() {
     let f;
-    expect(typeof f).toBe(__);
-    expect(typeof g).toBe(__);
+    expect(typeof f).toBe('undefined');
+    expect(typeof g).toBe('undefined');
     f = function g() {
       return 123;
     };
-    expect(typeof f).toBe(__);
-    expect(typeof g).toBe(__);
-    expect(f()).toBe(__);
+    expect(typeof f).toBe('function');
+    expect(typeof g).toBe('undefined');
+    expect(f()).toBe(123);
     let result;
     try {
       result = g();
     } catch (error) {
       result = 'error';
     }
-    expect(result).toBe(__);
+    expect(result).toBe('error');
     if (typeof g !== 'undefined') {
-      expect(f === g).toBe(__);
+      expect(f === g).toBe(true);
     }
   });
   test('4 - should understand function declaration', function() {
-    let result = 1;
+    let result = 1; // primitive value
     if (function f() {}) {
       result += typeof f;
     }
-    expect(result).toBe(__);
+    expect(result).toBe('1undefined');
   });
   test('5 - should understand function expression and declaration', function() {
     const f = function factorial(number) {
@@ -55,8 +55,8 @@ describe('function expression and function declaration', function() {
     } catch (error) {
       result = 'error';
     }
-    expect(result).toBe(__);
-    expect(typeof factorial).toBe(__);
+    expect(result).toBe(6);
+    expect(typeof factorial).toBe('undefined');
   });
   test('6 - should understand the impact of anonymous functions on stack traces', function() {
     //uncomment the line below and run the test; check the call stack
