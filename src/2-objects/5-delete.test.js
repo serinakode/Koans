e881b8e0764f4aa82573ccaab358e962775f6a8b
Serina Ko <serinako08@gmail.com>
2018-12-17 12:16:49 +1100

    completed

--- src/2-objects/5-delete.test.js
+++ src/2-objects/5-delete.test.js
@@ -11,8 +11,8 @@ describe('Delete', function() {
   });
   test('1 - should understand delete operator', function() {
     const result = delete samurai.name;
-    expect(result).toBe(__);
-    expect(samurai.name).toBe(__);
+    expect(result).toBe(true);
+    expect(samurai.name).toBe();
   });
   test('2 - should understand delete operator vs. setting property value to undefined', function() {
     const object = {
@@ -20,30 +20,30 @@ describe('Delete', function() {
     };
     let properties = '';
     object.propertyName = undefined;
-    expect(object.propertyName).toBe(__);
+    expect(object.propertyName).toBe(undefined);
     for (const name in object) {
       properties += name;
     }
-    expect(properties).toBe(__);
+    expect(properties).toBe('propertyName');
 
     properties = '';
     delete object.propertyName;
-    expect(object.propertyName).toBe(__);
+    expect(object.propertyName).toBe(undefined);
     for (const name in object) {
       properties += name;
     }
-    expect(properties).toBe(__);
+    expect(properties).toBe('');
     //Discuss this with your pair
     //Imagine using object as a read through cache - how would you manage the size of the cache?
   });
   test('3 - should understand delete operator', function() {
     samurai.samurai = samurai;
     delete samurai.samurai.samurai;
-    expect(samurai.samurai).toBe(__);
+    expect(samurai.samurai).toBe(undefined);
   });
   test('4 - should understand delete operator', function() {
     samurai.samurai = samurai;
     delete samurai.samurai.name;
-    expect(samurai.name).toBe(__);
+    expect(samurai.name).toBe(undefined);
   });
 });
