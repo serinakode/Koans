d3341ff9dc57018c9f42878f32c2ac61d6c532ac
Serina Ko <serinako08@gmail.com>
2018-12-19 10:17:28 +1100

    completed

--- src/4-arrays/7-spread.test.js
+++ src/4-arrays/7-spread.test.js
@@ -2,6 +2,6 @@ describe('Spread operator', function() {
   test('1 - should understand spread operator (...)', function() {
     const first = [1, 2, 3];
     const second = [6, 7, 8];
-    expect([...first, 4, 5, ...second]).toEqual(__);
+    expect([...first, 4, 5, ...second]).toEqual([1, 2, 3, 4, 5, 6, 7, 8]);
   });
 });
