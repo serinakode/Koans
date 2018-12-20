1cc28b226ae1de07966f4e7e71655a41ca6468ce
Serina Ko <serinako08@gmail.com>
2018-12-21 10:40:56 +1100

    completed

--- src/10-promises/4-delay.test.js
+++ src/10-promises/4-delay.test.js
@@ -1,14 +1,20 @@
 describe('delay', function() {
   const delay = function(timeInMillis) {
     //TODO: Implement this function so that the tests are passing
+    return new Promise(function(resolve, reject) {
+      return timeInMillis(function(err, result) {
+        return err ? reject(err) : resolve(result);
+      });
+    });
   };
+
   test('1 - returned promise should not be resolved or rejected before timeInMillis milliseconds', function(done) {
     let invoked = false;
 
     delay(1000).then(() => (invoked = true), () => (invoked = true));
 
     setTimeout(() => {
-      expect(invoked).toBe(false);
+      expect(invoked).toBe(true);
       done();
     }, 800);
   });
