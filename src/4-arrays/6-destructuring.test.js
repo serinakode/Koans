465928b71af70f7a92b63434218f0aa1a3e4e9df
Serina Ko <serinako08@gmail.com>
2018-12-20 13:40:38 +1100

    completed

--- src/4-arrays/6-destructuring.test.js
+++ src/4-arrays/6-destructuring.test.js
@@ -2,13 +2,13 @@ describe('Destructuring', function() {
   test('1 - should understand array destructuring', function() {
     const samurai = ['Myamoto', 'Hattori', 'Dave'];
     const [chief, assistant] = samurai;
-    expect(chief).toBe(__);
-    expect(assistant).toBe(__);
+    expect(chief).toBe('Myamoto');
+    expect(assistant).toBe('Hattori');
   });
   test('1 - should understand array destructuring & rest operator', function() {
     const samurai = ['Myamoto', 'Hattori', 'Dave'];
     const [chief, ...others] = samurai;
-    expect(chief).toBe(__);
-    expect(others).toEqual(__);
+    expect(chief).toBe('Myamoto');
+    expect(others).toEqual(['Hattori', 'Dave']);
   });
 });
