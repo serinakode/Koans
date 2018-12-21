1d0070c61445065ceace3282d89e159931fb012c
Serina Ko <serinako08@gmail.com>
2018-12-21 11:17:27 +1100

    completed

--- src/10-promises/6-all.test.js
+++ src/10-promises/6-all.test.js
@@ -5,7 +5,7 @@ const fetch = require('node-fetch');
 const serve = require('./serve');
 
 describe('Promise.all', function() {
-  const port = 3006;
+  const port = 3008;
   serve(port);
   const get = url =>
     fetch(`http://localhost:${port}/${url}`).then(response => response.json());
@@ -14,13 +14,27 @@ describe('Promise.all', function() {
     return Promise.all([
       get('data/player/1.json'),
       get('data/player/7.json')
-    ]).then(result => expect(result).toEqual(__));
+    ]).then(result =>
+      expect(result).toEqual([
+        {
+          name: 'Alice'
+        },
+        {
+          name: 'Erin'
+        }
+      ])
+    );
   });
 
+  // address already in use issue
   test('should understand how Promise.all deals with rejections', function() {
     return Promise.all([
       get('data/player/brake-it.json'),
       get('data/player/7.json')
-    ]).catch(reason => expect(reason).toEqual(__));
+    ]).catch(reason =>
+      expect(reason).toEqual(null, {
+        name: 'Erin'
+      })
+    );
   });
 });
