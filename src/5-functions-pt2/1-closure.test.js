30721d1a3f06b0a4f3a201993f8121e3227e4660
Serina Ko <serinako08@gmail.com>
2019-01-06 18:35:37 +1100

    completed

--- src/5-functions-pt2/1-closure.test.js
+++ src/5-functions-pt2/1-closure.test.js
@@ -1,35 +1,39 @@
 describe('Warmup - timers and asynchronous specs', function() {
-  test('0 - should understand why we need asynchronous specs (so that this spec doesnt just pass)', function() {
+  test('0 - should understand why we need asynchronous specs (so that this spec doesnt just pass)', function(done) {
     setTimeout(function() {
-      expect(1).toBe(2);
+      // setTimeout will be executed immediately, but function will be in the queue.
+      expect(1).toBe(1);
+      done();
     }, 100);
   });
+
   test('1 - should understand timers', function(done) {
     let i = 0;
     setTimeout(function() {
       i = 1;
     }, 200);
     setTimeout(function() {
-      expect(i).toBe(__);
+      expect(i).toBe(0);
     }, 100);
     setTimeout(function() {
-      expect(i).toBe(__);
+      expect(i).toBe(1);
       done();
     }, 300);
-    expect(i).toBe(__);
+    // the above functions are still in the queue. not executed yet.
+    expect(i).toBe(0);
   });
   test('2 - should understand timers', function(done) {
     let i = 0;
-    expect(i).toBe(__);
+    expect(i).toBe(0);
     setTimeout(function() {
       i = 1;
     }, 0);
-    expect(i).toBe(__);
+    expect(i).toBe(0);
     setTimeout(function() {
-      expect(i).toBe(__);
+      expect(i).toBe(1);
       done();
     }, 1);
-    expect(i).toBe(__);
+    expect(i).toBe(0);
   });
   test('3 - should understand timers', function(done) {
     let i = 0;
@@ -38,17 +42,19 @@ describe('Warmup - timers and asynchronous specs', function() {
       i = 1;
     }, 300);
     while (new Date().getTime() <= loopDueTime) {
-      //intentionally empty
+      // intentionally empty
     }
-    expect(i).toBe(__);
+    // cos while loops taking up some time, this function is executed first
+    expect(i).toBe(0);
     setTimeout(function() {
-      expect(i).toBe(__);
+      expect(i).toBe(1);
       done();
     }, 0);
-    expect(i).toBe(__);
+    expect(i).toBe(0);
   });
 });
 
+// skip this
 describe('Closure', function() {
   test('1 - should understand loop and closure', function(done) {
     let i;
@@ -59,7 +65,7 @@ describe('Closure', function() {
       }, 100 * (i + 1));
     }
     setTimeout(function() {
-      expect(result).toBe(__);
+      expect(result).toBe('100, 200, 300');
       done();
       //discuss this with your pair
       //now change the code within setTimeout (and within setTimeout only) so that expected result is '123'
